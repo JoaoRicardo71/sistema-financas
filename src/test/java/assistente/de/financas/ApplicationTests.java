1dd58c64209fcde2fa28d5e3009b9caabf55f5b1
JoaoRicardo71 <825156045@ulife.com.br>
2026-04-09 23:12:32 -0300

    primeiro commit

---
+++ src/test/java/assistente/de/financas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package assistente.de.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
